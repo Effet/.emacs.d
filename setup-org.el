82b1df5476c887d356c72db9326a3e104f1a2e30
Catl Sing <nesuadark@gmail.com>
2013-08-08 17:50:14 +0800

    org file link always use relative path

--- setup-org.el
+++ setup-org.el
@@ -2,5 +2,14 @@
 (setq org-startup-indented t)
 (setq org-startup-folded 'content)
 (setq org-src-fontify-natively t)
+(setq org-link-file-path-type 'relative)
+
+(defun org-time-stamp-setting ()
+  (setq time-stamp-pattern
+        "8/DATE:[ \t]+\\\\?[\"<]%Y-%02m-%02d\\\\?[\">]")
+  (add-hook 'before-save-hook 'time-stamp))
+
+(add-hook 'org-mode-hook
+          'org-time-stamp-setting)
 
 (provide 'setup-org)
