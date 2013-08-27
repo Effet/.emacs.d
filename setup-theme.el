c032e0a7fc7c0d2c58e06d946acfe8d7a547d609
ironyKuU <nesuadark@gmail.com>
2013-08-28 02:42:43 +0800

    use my modified theme

--- setup-theme.el
+++ setup-theme.el
@@ -1,4 +1,5 @@
 (setq custom-theme-directory (concat user-emacs-directory "themes"))
-(if window-system (load-theme 'base16-monokai t))
+;; (if window-system (load-theme 'base16-monokai t))
+(load-theme 'kuu t)
 
 (provide 'setup-theme)
