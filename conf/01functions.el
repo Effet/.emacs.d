09453c6cedacedf4e1054c3e43a0fc1a306f9a72
ironyKuU <nesuadark@gmail.com>
2013-09-01 16:35:57 +0800

    add uniq-lines

--- conf/01functions.el
+++ conf/01functions.el
@@ -13,6 +13,27 @@
                        (if (frame-parameter nil 'fullscreen) nil 'fullboth)))
 
 
+;; http://yesybl.org/blogen/?p=25
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
+
 ;; https://github.com/magnars/.emacs.d/blob/master/defuns/misc-defuns.el#L20-#L27
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
