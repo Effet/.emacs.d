e091984140faf411c094efaab1c751d7d575bbc4
Effet <nesuadark@gmail.com>
2013-11-25 17:18:21 +0800

    change the management style

---
+++ init-modules.el
@@ -0,0 +1,5 @@
+(load "init-smartparens")
+;; (load "init-auto-complete")
+(load "init-company")
+(load "init-helm")
+(load "init-yasnippet")
--- init.el
+++ init.el
@@ -11,21 +11,42 @@
   (when (file-directory-p project)
     (add-to-list 'load-path project)))
 
-
-;; configs
-(mapc 'load (directory-files "~/.emacs.d/conf" t "\\.el$"))
+;;; Core
+(load "init-package")
+(load "init-functions")
+(load "init-core")
+(load "init-locale")
+(load "init-appareance")
+(load "init-bindings")
 
-
-;; modules
-(defvar modules-dir
-  (expand-file-name "modules" user-emacs-directory))
-
-(add-to-list 'load-path modules-dir)
-
+;;; Modules
+(load "init-abbrev")
+(load "init-ack-grep")
+(load "init-auto-complete")
+(load "init-cedet")
+(load "init-company")
+(load "init-dired")
+(load "init-emacs-lisp-mode")
+(load "init-eshell")
+(load "init-gtags")
+(load "init-helm")
+(load "init-ibuffer")
+(load "init-ido")
+(load "init-magit")
+(load "init-org")
 (load "init-smartparens")
+(load "init-smex")
+(load "init-term")
+(load "init-vc")
+(load "init-window")
+(load "init-yasnippet")
+(load "init-misc")
 
-
-;; Emacs server
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+;;; Programming Languages
+(load "init-geiser")
+(load "init-cc-mode")
+(load "init-java-mode")
+(load "init-js-mode")
+(load "init-latex-mode")
+(load "init-python-mode")
+(load "init-web-mode")
